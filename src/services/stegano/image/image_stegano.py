5538bd3795a6330189a819c78ec45063c240b260
dikaa <13518007@std.stei.itb.ac.id>
2021-10-01 13:42:51 +0700

    change flag

--- src/services/stegano/image/image_stegano.py
+++ src/services/stegano/image/image_stegano.py
@@ -19,47 +19,45 @@ def read_cover_image(filename):
 
 def encrypt_steg_image(cover, message, seq = True, key = "STEGA"):
     input_length = len(message)
+
     if seq:
         # Add flag for seq with 7 at end, 25x
         for i in range(25):
             x = cover[i] // 10 * 10 + 7
-            cover[i] = x - 10 if x > 255 else x
-        
-        # steg message
-        for i in range(25,input_length+25):
-            cover[i] = (cover[i] & ~1) | message[i-25]
-
-        # Add flag for end message with 9 at end, 25x
-        for j in range(25):
-            i = input_length + 25 + j
-            x = cover[i] // 10 * 10 + 9
-
-            cover[i] = x - 10 if x > 255 else x
-
+            cover[i] = x - 10 if x > 255 else x        
     else:
         # Add flag for random with 8 at begin, 25x
         for i in range(25):
             x = cover[i] // 10 * 10 + 8
             cover[i] = x - 10 if x > 255 else x
+    
+    # Add message length in next 10 bit, so max input length 9.999.999.999
+    num_len = [int(x) for x in str(input_length)]
+    z = 10 - len(num_len)
+    for j in range(10):
+        i = 25 + j
+        if(j < z):
+            x = cover[i] // 10 * 10 + 0
+            cover[i] = x - 10 if x > 255 else x
+        else:
+            x = cover[i] // 10 * 10 + num_len[j - z]
+            cover[i] = x - 10 if x > 255 else x
+        print(cover[i], i)
 
-        # Add input length in next 10 bit, so max input length 9.999.999.999
-        num_len = [int(x) for x in str(input_length)]
-        z = 10 - len(num_len)
-        for j in range(10):
-            i = 25 + j
-            if(j < z):
-                x = cover[i] // 10 * 10 + 0
-                cover[i] = x - 10 if x > 255 else x
-            else:
-                x = cover[i] // 10 * 10 + num_len[j - z]
-                cover[i] = x - 10 if x > 255 else x
-
+    # Steg message in image
+    if seq:
+        # steg message
+        for i in range(35,input_length+35):
+            cover[i] = (cover[i] & ~1) | message[i-35]
+    else:
         # generate random post
         count = 0
         for i in key:
             count += ord(i)
         random.seed(count)
         pos = random.sample(range(35, len(cover)), input_length)
+
+        # steg message
         idx = 0
         for i in pos:
             cover[i] = message[idx]
@@ -73,29 +71,25 @@ def decrypt_stegano_image(image, key = "STEGA"):
     for i in range(25):
         identifier.append(image[i] % 10)
 
-    if(identifier.count(7) == len(identifier)):
-        # stegano using sequential
-        count_flag = 0
-        i = 25
-        length_data = len(image) - 25
-        while count_flag < 25 and i < length_data:
-            result.append(image[i] % 2)
-            if  image[i] % 10 == 9:
-                count_flag += 1
-            else:
-                count_flag = 0
-            i += 1
-        # delete flag from message
-        result = result[:-25]
-    elif(identifier.count(8) == len(identifier)):
-        # stegano using random
-        input_length = int(''.join([str(x % 10) for x in image[25:35]]))
+    # identify lenght of message in byte 25-35
+    input_length = int(''.join([str(x % 10) for x in image[25:35]]))
+    print(image[25:35])
 
+    # extract message
+    if(identifier.count(7) == len(identifier)):
+        # using sequential
+        for i in range(input_length):
+            result.append(image[i+35] % 2)
+    elif(identifier.count(8) == len(identifier)):
+        # using random
+        # get position of steg byte
         count = 0
         for i in key:
             count += ord(i)
         random.seed(count)
         pos = random.sample(range(35, len(image)), input_length)
+
+        #extract 
         idx = 0
         for i in pos:
             result.append(image[i] % 2)
